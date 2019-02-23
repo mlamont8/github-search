924c801ff141b5376a275b46f96dbfac8941771f
mlamont8 <mlamont8@hotmail.com>
2019-02-22 21:17:34 -0500

    updated pagination

--- src/App.js
+++ src/App.js
@@ -15,23 +15,35 @@ class App extends Component {
     this.state = {
       searchTerm: "",
       pageNumber: 1,
-      gridData: null
+      gridData: null,
+      totalResults: 0,
+      resultsPerPage: 12
     };
 
     this.handleSearch = this.handleSearch.bind(this);
+    this.onPageChange = this.onPageChange.bind(this);
+  }
+
+  onPageChange(page) {
+    this.setState({
+      pageNumber: page
+    });
+    this.handleSearch(this.state.searchTerm);
   }
 
   async handleSearch(result) {
     const url = `https://api.github.com/search/users?q=${result}&page=${
       this.state.pageNumber
-    }&per_page=12`;
+    }&per_page=${this.state.resultsPerPage}`;
+    console.log(url);
 
     try {
       const response = await axios.get(url);
-      console.log(response.data.items);
+      console.log(response.data);
       this.setState({
         gridData: response.data.items,
-        searchTerm: result
+        searchTerm: result,
+        totalResults: response.data.total_count
       });
     } catch (error) {
       console.error(error);
@@ -45,7 +57,13 @@ class App extends Component {
         <Appbar />
         <main>
           <Search handleSearch={this.handleSearch} />
-          <Pagination />
+          <Pagination
+            resultsPerPage={this.state.resultsPerPage}
+            totalResults={this.state.totalResults}
+            pageNumber={this.state.pageNumber}
+            onPageChange={this.onPageChange}
+          />
+
           <Results gridData={this.state.gridData} />
         </main>
         <footer>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,38 +1,15 @@
 import React, { Component } from "react";
+import PagePagination from "rc-pagination";
+import "rc-pagination/assets/index.css";
 
-import TablePagination from "@material-ui/core/TablePagination";
-
-// Material UI Pagination required to be within a table to avoid errors
-
-class Pagination extends Component {
-  handleChangePage(e, page) {
-    console.log("Page Change Request", e, page);
-  }
-
-  render() {
-    return (
-      <div className="paginationContainer">
-        <table>
-          <tbody>
-            <tr>
-              <TablePagination
-                rowsPerPage={12}
-                rowsPerPageOptions={[12]}
-                labelRowsPerPage={"Results per page"}
-                colSpan={3}
-                count={100}
-                page={1}
-                SelectProps={{
-                  native: true
-                }}
-                onChangePage={this.handleChangePage}
-              />
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+const Pagination = props => (
+  <div className="paginationContainer">
+    <PagePagination
+      onChange={props.onPageChange}
+      current={props.pageNumber}
+      total={props.totalResults}
+    />
+  </div>
+);
 
 export default Pagination;
